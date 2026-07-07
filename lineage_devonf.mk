a2ba0bbf081f874a725dc29d899cd7d7c8054339
Rajkumar Saini <287562898+rajkumar-saini@users.noreply.github.com>
2026-07-07 17:17:18 +0530

    devonf: Adapt to LineageOS

--- lineage_devonf.mk
+++ lineage_devonf.mk
@@ -11,15 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/motorola/devonf/device.mk)
 
-# Inherit some common YAAP stuff.
-$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := yaap_devonf
+PRODUCT_NAME := lineage_devonf
 PRODUCT_DEVICE := devonf
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := moto g73 5G 
+PRODUCT_MODEL := moto g73 5G
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
